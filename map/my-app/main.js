87a986a91237a1212ad772a797eb720f5157cdf0
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-02-25 16:27:57 +0100

    Localización inicial en Bruselas

--- map/my-app/main.js
+++ map/my-app/main.js
@@ -2,6 +2,7 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj';
 
 const map = new Map({
   target: 'map',
@@ -11,7 +12,7 @@ const map = new Map({
     })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([4.3525, 50.846667]),
+    zoom: 15
   })
 });
