3314beffc1a7a773a7c5ce2b64c54493a0eb66bd
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-02-25 20:26:22 +0100

    Se obtienen las coordenadas al hacer click en el mapa

--- map/my-app/main.js
+++ map/my-app/main.js
@@ -3,7 +3,7 @@ import './style.css';
 import {Map, View, Feature} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import {fromLonLat} from 'ol/proj';
+import {fromLonLat, toLonLat} from 'ol/proj';
 import {Vector as LayerVector} from 'ol/layer';
 import {Vector as SourceVector} from 'ol/source';
 import {Point} from 'ol/geom';
@@ -11,13 +11,13 @@ import {Icon, Style} from 'ol/style.js';
 
 // Las coordenadas falsas a añadir
 const coordinates_info = [
-  [[4.350, 50.841667], true, 1],
-  [[4.351, 50.843667], true, 2],
-  [[4.352, 50.845667], true, 3],
-  [[4.353, 50.847667], false, 1],
-  [[4.354, 50.849667], false, 2],
-  [[4.355, 50.842667], false, 3],
-  [[4.356, 50.844667], true, 4],
+  [[4.350, 50.841667], true, 1, "Uno"],
+  [[4.351, 50.843667], true, 2, "Dos"],
+  [[4.352, 50.845667], true, 3, "Tres"],
+  [[4.353, 50.847667], false, 1, "Cuatro"],
+  [[4.354, 50.849667], false, 2, "Cinco"],
+  [[4.355, 50.842667], false, 3, "Seis"],
+  [[4.356, 50.844667], true, 4, "Siete"],
 ];
 
 // Función que crea el objeto marcador
@@ -67,3 +67,13 @@ for (const coordinate of coordinates_info) {
   });
   map.addLayer(new_layer);
 }
+
+// Al hacer click en el mapa se obtiene la información de las coordenadas
+map.on(
+  'click',
+  function (evt) {
+    const coordinate = evt.coordinate;
+    const coordinate_str = toLonLat(coordinate);
+    console.log(coordinate_str);
+  }
+)
