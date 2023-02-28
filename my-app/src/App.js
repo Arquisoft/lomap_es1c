356bda5ef84d744daf095cb3aa274215e9213114
UO283970 <98962779+UO283970@users.noreply.github.com>
2023-02-28 19:53:18 +0100

    mapa y pruebas con marcadores

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,54 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import Geolocation from '@react-native-community/geolocation';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Buenos dias</p>
-    </div>
-  );
+export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map style={{position:"none"}}/>;
 }
 
-export default App;
+function Map() {
+  const [latitude, setLatitude] = React.useState('');
+  const [longitude, setLongitude] = React.useState('');
+
+  Geolocation.getCurrentPosition((position) =>{
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude)
+  }
+  );
+
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const center = ({ lat: Number(latitude), lng: Number(longitude) });
+
+  return (
+    <GoogleMap zoom={13} center={center} mapContainerClassName="map-conteiner" onClick={e => onMapClick(e)}>
+      {markers.map((marker) => (
+          <Marker
+            key={`${marker.lat}-${marker.lng}`}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+    </GoogleMap>
+  );
+}
