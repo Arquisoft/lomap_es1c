4ba86c3c907b575012e1b9187b7e590d02d3b3c2
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-03-05 21:50:31 +0100

    Prueba mostrar datos en lista

--- my-app/src/Sidebar/InicioTabContent.js
+++ my-app/src/Sidebar/InicioTabContent.js
@@ -1,10 +1,47 @@
 import React from "react"
+import LugarCard from "./LugarCard"
+
+// TODO: eliminar datos hard-codeados
+const lugares = [
+    {  
+        id: 1,
+        titulo: "Ayto",
+        coor: (10, 10),
+        puntuacion: 4.9,
+        comentarios: [],
+        fotos: []
+    },
+    {
+        id:2,
+        titulo: "Niemeyer",
+        coor: (10, 10),
+        puntuacion: 4.9,
+        comentarios: ["Un sitio bien chulo"],
+        fotos: []
+    },
+    { 
+        id: 3,
+        titulo: "Parque",
+        coor: (150, 10),
+        puntuacion: 3.0,
+        comentarios: ["Solo un parque", "¿Qué más puedes pedir?"],
+        fotos: []
+    }
+  ]
 
 export default function InicioTabContent(props) {
+    const cards = lugares.map(
+        lugar =>
+        <LugarCard
+            key={lugar.id}
+            {...lugar}
+        />
+    )
+
     return (
         <div id="Inicio" className="tabcontent">
             <h1>El contenido de inicio</h1>
-            <p>El contenido de inicio</p>
+            {cards}
         </div>
     )
 }
---
+++ my-app/src/Sidebar/LugarCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LugarCard(props) {
+    return (
+        <div className="card">
+            <p>{props.titulo}</p>
+        </div>
+    )
+}
