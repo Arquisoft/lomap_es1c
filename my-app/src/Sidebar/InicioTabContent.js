fd8431a59a0922d25b5d5e3679730531c56516f0
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-03-09 14:40:17 +0100

    Añado barra de búsqueda para lugares en el sidebar

--- my-app/src/Sidebar/InicioTabContent.js
+++ my-app/src/Sidebar/InicioTabContent.js
@@ -2,17 +2,36 @@ import React from "react"
 import PlaceCard from "./PlaceCard"
 
 export default function InicioTabContent(props) {
-    const cards = props.userPlaces.map(
+    const [cards, setCards] = React.useState(props.userPlaces.map(
         place =>
         <PlaceCard
             key={place.id}
             {...place}
         />
-    )
+    ))
+    
+    function textToSearchChange(event) {
+        setCards(
+            props.userPlaces.filter(
+                place => place.name.toLowerCase().includes(event.target.value.toLowerCase())
+            ).map(
+                place =>
+                <PlaceCard
+                    key={place.id}
+                    {...place}
+                />
+            )
+        )
+    }
 
     return (
         <div id="Inicio" className="tabcontent">
-            <h1>El contenido de inicio</h1>
+            <h1>Tus lugares</h1>
+            <input
+                type="text"
+                placeholder="Buscar"
+                onChange={textToSearchChange}
+            ></input>
             {cards}
         </div>
     )
