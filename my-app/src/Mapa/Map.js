16e1baccb4abd59a14497097ebb1720744062bcd
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-03-25 20:03:55 +0100

    Mapa se pone en modo oscuro

--- my-app/src/Mapa/Map.js
+++ my-app/src/Mapa/Map.js
@@ -1,10 +1,96 @@
 import React from "react";
 import { GoogleMap, useLoadScript, Marker, MarkerF } from "@react-google-maps/api";
+import { Themes, ThemeContext } from '../contexts/ThemeContext';
 import Geolocation from '@react-native-community/geolocation';
 import Button from '@mui/material/Button';
+import { useContext } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 //import {addPlace,getPlaces} from '../Places/Places';
 //import PlaceConst from "../Places/Place";
 
+const darkMapStyle = [
+  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ color: "#263c3f" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#6b9a76" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#38414e" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#212a37" }],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#9ca5b3" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#746855" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#1f2835" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#f3d19c" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#2f3948" }],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#17263c" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#515c6d" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#17263c" }],
+  },
+];
+
+const lightMapStyle = [];
 
 export default function CreateMap({open,setLatitude,setLongitude,markers,setMarkers,buttonState,setStateButton,places}) {
     const { isLoaded } = useLoadScript({
@@ -79,6 +165,13 @@ function Map({openModal,setLongitudeMark,setLatitudeMark,markersState,setMarkers
     openModal(true);
   }
 
+  const { currentTheme } = useContext(ThemeContext);
+  const [currentMapStyle, setCurrentMapStyle] = useState();
+
+  useEffect(() => {
+    setCurrentMapStyle(currentTheme === Themes.LIGHT ? lightMapStyle : darkMapStyle);
+  }, [currentTheme]);
+
   //Nos devuelve el mapa con todos los componentes asociados.
   return (
     <div>
@@ -96,6 +189,7 @@ function Map({openModal,setLongitudeMark,setLatitudeMark,markersState,setMarkers
         center={center}
         mapContainerClassName="map-conteiner"
         onClick={e => onMapClick(e)}
+        options={{styles: currentMapStyle}}
       >
         {markers.map((marker) => (
           <Marker
