75897374f8f4ded5cc3ed8a89794153432400482
Miguel <migueelsuarez@gmail.com>
2023-03-28 01:39:20 +0200

    Organización y cambios en las rutas de los location

--- backend/routes/LocationRoutes.js
+++ backend/routes/LocationRoutes.js
@@ -1,15 +1,19 @@
 module.exports = function (app, locationController) {
     
-    app.get('/location/:id', locationController.getLocation);
     app.get('/location', locationController.getAllLocations);
 
-    app.post('/location', locationController.createLocation);
-    app.put('/location/:id', locationController.updateLocation);
-    app.delete('/:id', locationController.deleteLocation);
+    
+    app.get('/location/categories',locationController.getCategories);
 
-    app.put('/location/:id/rating',locationController.setRating);
+    app.get('/location/:id', locationController.getLocation);
+    
+    app.delete('/location/:id', locationController.deleteLocation);
+    
+    app.put('/location/:id', locationController.updateLocation);
+    app.post('/location/:id/rating',locationController.setRating);
+    app.post('/location', locationController.createLocation);
     app.post('/location/:id/comment', locationController.addComment);
     app.post('/location/:id/photos', locationController.addPhoto);
 
-    app.get('/location/categories',locationController.getCategories);
+    
 }
