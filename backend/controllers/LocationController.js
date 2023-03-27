29091e11d65cd13e1ef600be3dfe01f8fbe69b5a
Miguel <migueelsuarez@gmail.com>
2023-03-28 01:22:26 +0200

    Cambios en el controlador y rutas de Location

--- backend/controllers/LocationController.js
+++ backend/controllers/LocationController.js
@@ -1,67 +1,50 @@
 const { getSessionFromStorage} = require('@inrupt/solid-client-authn-node');
-//Importamos los objetos del Modelo
+
 const Location = require('../models/Location');
-const { Review } = require('../models/Review');
-//Utilizamos nuestra clase solid para parsear.
-const solid = require('../solid/Solid.js');
+const solid = require('../solid/SolidPrueba.js');
 
-//localizaciones hardcodeadas
-
-const location1 = new Location('1', 'Parque San Francisco', 'El Palomar', 43.361444061368786, -5.850460182754155, 'Parque', [{ rating: 4, comment: 'Lugar muy bonito' }], ['https://www.example.com/photo1.jpg']);
-const location2 = new Location('2', 'Catedral de León', 'Plaza Regla, s/n, 24003 León', 42.599022562675496, -5.56743789804147, 'Monumento', [{ rating: 5, comment: 'Espectacular' }, { rating: 4, comment: 'Muy bonita' }], ['https://www.example.com/photo2.jpg', 'https://www.example.com/photo3.jpg']);
-const location3 = new Location(null, 'Playa de San Lorenzo', 'Paseo de las Palmeras, s/n, 33203 Gijón, Asturias', 43.543158258415634, -5.669035703728212, 'Playa', [], ['https://www.example.com/photo4.jpg']);
-const location4 = new Location('4', 'Parque del Retiro', 'Plaza de la Independencia, s/n, 28001 Madrid', 40.4152419510136, -3.686089362482189, 'Parque', [{ rating: 4, comment: 'Muy bonito' }, { rating: 3, comment: 'Un poco masificado' }], ['https://www.example.com/photo5.jpg', 'https://www.example.com/photo6.jpg', 'https://www.example.com/photo7.jpg']);
-const location5 = new Location(null, 'Puerto Viejo de Algorta', 'Puerto Viejo, 48990 Getxo, Bizkaia', 43.35296326065165, -3.013914901236413, 'Puerto', [{ rating: 5, comment: 'Precioso' }], ['https://www.example.com/photo8.jpg', 'https://www.example.com/photo9.jpg']);
-
-const categories=["Restaurante","Punto de Interes","Tienda","Edificio público","Ocio"];
-const locations=[location1,location2,location3,location4,location5];
-
-  //TODO
   async function getLocation(req, res) {
-    const { url } = req.params;
+    const { id } = req.params;
     try {
+ 
       let session=await getSessionFromStorage(req.session.sessionId);
-      const location = await Solid.getLocationById(url);
+      const location1 = await solid.getLocationById(id);
+      
       res.send(JSON.stringify(location1));
     } catch (err) {
       res.status(404).json({ error: err.message });
     }
   }
-  //TODO
+
+
   async function getAllLocations(req, res) {
     try {
       session=await getSessionFromStorage(req.session.sessionId);
-      //solid.getAllLocations(session);
-      //const locations = await Solid.getAllLocations();
+      const locations=await solid.getAllLocations(session);
       res.send(JSON.stringify(locations));
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
   }
 
-
  async function createLocation(req, res) {
     const { name, address, latitude, longitude, category } = req.body;
     try {
       session=await getSessionFromStorage(req.session.sessionId);
-      //const location = new Location(name, address, latitude, longitude, category);
-
-      await solid.saveLocation(session,location1);
+      const location = new Location(name, address, latitude, longitude, category);
+      await solid.saveLocation(session,location);
       res.status(201).json(location);
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
   }
 
-
-  
-
   async function updateLocation(req, res) {
     const {id }=req.params;
     const { name, address, latitude, longitude, category } = req.body;
 
     try {
-      const location = await Solid.getLocationById(id);
+      const location = await solid.getLocationById(id);
       //Toma el valor nuevo si no es null.
       if (name) location.name = name;
       if (address) location.address = address;
@@ -69,7 +52,7 @@ const locations=[location1,location2,location3,location4,location5];
       if (longitude) location.longitude = longitude;
       if (category) location.category = category;
       //Luego vuelve a guardarlo
-      await Solid.saveLocation(location);
+      await solid.saveLocation(location);
       res.json(location);
     } catch (err) {
       res.status(404).json({ error: err.message });
@@ -79,13 +62,69 @@ const locations=[location1,location2,location3,location4,location5];
   async function deleteLocation(req, res) {
     const { id } = req.params;
     try {
-      await Solid.deleteLocationById(id);
+      await solid.deleteLocationById(id);
       res.sendStatus(204);
     } catch (err) {
       res.status(404).json({ error: err.message });
     }
   }
 
+  async function setRating(req,res){
+    const { id } = req.params;
+    const { rating } = req.body;
+
+    try {
+      const location = await Solid.getLocationById(id);
+      location.setRating(rating);
+      await solid.saveLocation(location);
+
+      res.status(201).json({ message: 'Rating changed successfully' });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+
+
+  async function addPhoto(req, res) {
+    const { id } = req.params;
+    const { photoUrl } = req.body;
+
+    try {
+      const location = await solid.getLocationById(id);
+      location.addPhoto(photoUrl);
+      await solid.saveLocation(location);
+
+      res.status(201).json({ message: 'Photo added successfully' });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+  async function addComment(req, res) {
+    const { id } = req.params;
+    const { photoUrl } = req.body;
+
+    try {
+      const location = await solid.getLocationById(id);
+      location.addComment(photoUrl);
+      await solid.saveLocation(location);
+
+      res.status(201).json({ message: 'Comment added successfully' });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+
+  async function getCategories(req,res){
+    console.log("controller")
+    try{
+      let categories=await solid.getCategories();
+      res.send(JSON.stringify(categories));
+    }catch (err){
+      res.status(400).json("Ha habido un problema devolviendo las categorias");
+    }
+  }
+
+    /*
   async function addReview(req, res) {
     const { id } = req.params;
     const { rating, text, author } = req.body;
@@ -101,30 +140,8 @@ const locations=[location1,location2,location3,location4,location5];
       res.status(400).json({ error: err.message });
     }
   }
-
-  async function addPhoto(req, res) {
-    const { id } = req.params;
-    const { photoUrl } = req.body;
-
-    try {
-      const location = await Solid.getLocationById(id);
-      location.addPhoto(photoUrl);
-      await Solid.saveLocation(location);
-
-      res.status(201).json({ message: 'Photo added successfully' });
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
-  }
-
-  async function getCategories(req,res){
-    try{
-      res.status(200).json(JSON.stringify(categories));
-    }catch (err){
-      res.status(400).json("Ha habido un problema devolviendo las categorias");
-    }
-  }
+  */
 
   module.exports={
-    createLocation,getAllLocations,getLocation,deleteLocation,updateLocation,addReview,addPhoto,getCategories
+    createLocation,getAllLocations,getLocation,deleteLocation,updateLocation,addPhoto,getCategories,setRating,addComment
   };
--- backend/routes/LocationRoutes.js
+++ backend/routes/LocationRoutes.js
@@ -1,12 +1,15 @@
 module.exports = function (app, locationController) {
-    app.post('/location', locationController.createLocation);
+    
+    app.get('/location/:id', locationController.getLocation);
     app.get('/location', locationController.getAllLocations);
 
-    app.get('/location/:id', locationController.getLocation);
+    app.post('/location', locationController.createLocation);
     app.put('/location/:id', locationController.updateLocation);
     app.delete('/:id', locationController.deleteLocation);
 
-    app.post('/location/:id/reviews', locationController.addReview);
+    app.put('/location/:id/rating',locationController.setRating);
+    app.post('/location/:id/comment', locationController.addComment);
     app.post('/location/:id/photos', locationController.addPhoto);
+
     app.get('/location/categories',locationController.getCategories);
 }
