d9bca70628a539bbc9cfa97645e506c065f9f77a
Miguel <migueelsuarez@gmail.com>
2023-03-28 01:21:03 +0200

    Creacion de modelo y controlador basicos de las rutas

--- backend/models/Route.js
+++ backend/models/Route.js
@@ -19,7 +19,10 @@ class Route {
     updateName(name){
         this.name=name;
     }
-
-    
+    generateRandomId() {
+        const randomIdentifier = Math.random().toString(36).substring(2, 8);
+        const currentDate = new Date().getTime();
+        return `${currentDate}_${randomIdentifier}`;
+      }
   }
   module.exports = Route;   
