13c05fc5f7d6df9bedbe64434ef188e48adb3fab
Damián Fernández Álvarez <UO283970@uniovi.es>
2023-03-28 09:21:16 +0200

    Para hacer merge

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './Login';
+import Login from './login';
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Login />
+    <App />
+    {/* <Login /> */}
   </React.StrictMode>
 );
 
