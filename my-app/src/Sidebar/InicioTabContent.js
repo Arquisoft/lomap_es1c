3d17fa338523ed617d785090973abb63bba96c4f
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-03-29 21:02:08 +0200

    El listado de lugares del sidebar se actualiza automáticamente

--- my-app/src/Sidebar/InicioTabContent.js
+++ my-app/src/Sidebar/InicioTabContent.js
@@ -1,41 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PlaceCard from "./cards/PlaceCard";
 import { useTranslation } from "react-i18next";
 
 export default function InicioTabContent(props) {
     const [t, i18n] = useTranslation("global")
-
-    const [cards, setCards] = React.useState(props.userPlaces.map(
-        place =>
-        <PlaceCard
-            key={place.id}
-            {...place}
-        />
-    ))
-    
-    function textToSearchChange(event) {
-        setCards(
-            props.userPlaces.filter(
-                place => place.name.toLowerCase().includes(event.target.value.toLowerCase())
-            ).map(
-                place =>
-                <PlaceCard
-                    key={place.id}
-                    {...place}
-                />
-            )
-        )
-    }
+    const [searchBarText, setSearchBarText] = useState("")
 
     return (
         <div id="Inicio" className="tabcontent">
+            <p>{props.userPlaces.length}</p>
             <h1>{t("sidebar.tabs.start-content.title")}</h1>
             <input
                 type="search"
                 placeholder={t("sidebar.tabs.start-content.search-bar-placeholder")}
-                onChange={textToSearchChange}
+                onChange={(e) => (setSearchBarText(e.target.value.toLowerCase()))}
             ></input>
-            {cards}
+            {props.userPlaces.filter(place => place.name.toLowerCase().includes(searchBarText)).map(place =><PlaceCard key={place.id} {...place}/>)}
         </div>
     )
 }
