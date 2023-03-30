cf32819bf95de662caa1408e1abef135529b2ccf
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-03-30 12:47:22 +0200

    Se obtienen las rutas de la restapi Closes #110

--- my-app/src/Sidebar/cards/RutaCard.js
+++ my-app/src/Sidebar/cards/RutaCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function RutaCard(props) {
     return (
         <div className="card">
-            <h3>{props.titulo}</h3>
+            <h3>{props.name}</h3>
             <hr />
         </div>
     )
