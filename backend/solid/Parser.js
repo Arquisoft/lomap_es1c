8b07c1b05f4198e7bd507ab716fb0959c79716e2
UO284647 <uo284647@uniovi.es>
2023-04-02 16:25:37 +0200

    Creacion parser.js
    Creacion parser.js

---
+++ backend/solid/Parser.js
@@ -0,0 +1,32 @@
+
+const Location = require('../models/Location');
+const Review = require('../models/Review');
+const Route = require('../models/Route');
+const Foto = require('../models/Foto');
+
+
+
+
+
+
+function parseLocation(location){
+
+}
+
+
+function parseReview(review){
+
+}
+
+function parseFoto(foto){
+
+}
+
+function parseRoute(route){
+
+}
+
+
+module.exports={
+    parseLocation, parseReview, parseFoto, parseRoute
+  };
