d94d4db27b963624bf6f39d1922f87c97d297eb5
UO284647 <uo284647@uniovi.es>
2023-04-02 16:23:45 +0200

    Creacion serializer.js
    Creacion serializer.js

---
+++ backend/solid/Serializer.js
@@ -0,0 +1,32 @@
+
+const Location = require('../models/Location');
+const Review = require('../models/Review');
+const Route = require('../models/Route');
+const Foto = require('../models/Foto');
+
+
+
+
+
+
+function serializeLocation(location){
+
+}
+
+
+function serializeReview(review){
+
+}
+
+function serializeFoto(foto){
+
+}
+
+function serializeRoute(route){
+
+}
+
+
+module.exports={
+    serializeLocation, serializeReview, serializeFoto, serializeRoute
+  };
