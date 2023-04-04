f188cbac5f73aeb7ef943ef5ec95201f88113b3d
Miguel <migueelsuarez@gmail.com>
2023-04-04 13:55:25 +0200

    Rutas actualizadas de acuerdo a lo establecido

--- backend/routes/LocationRoutes.js
+++ backend/routes/LocationRoutes.js
@@ -1,19 +1,20 @@
 module.exports = function (app, locationController) {
-    
-    app.get('/location', locationController.getAllLocations);
+	app.get("/location", locationController.getAllLocations);
 
-    
-    app.get('/location/categories',locationController.getCategories);
+	//Categorias
+	app.get("/location/categories", locationController.getCategories);
+	app.get(
+		"location/categories/:name",
+		locationController.getLocationsByCategorie
+	);
 
-    app.get('/location/:id', locationController.getLocation);
-    
-    app.delete('/location/:id', locationController.deleteLocation);
-    
-    app.put('/location/:id', locationController.updateLocation);
-    app.post('/location/:id/rating',locationController.setRating);
-    app.post('/location', locationController.createLocation);
-    app.post('/location/:id/comment', locationController.addComment);
-    app.post('/location/:id/photo', locationController.addPhoto);
-
-    
-}
+	//Crud
+	app.get("/location/:id", locationController.getLocation);
+	app.delete("/location/:id", locationController.deleteLocation);
+	app.put("/location/:id", locationController.updateLocation);
+	app.post("/location", locationController.createLocation);
+	//Arrays
+	app.post("/location/:id/rating", locationController.addReview);
+	app.post("/location/:id/comment", locationController.addComment);
+	app.post("/location/:id/photo", locationController.addPhoto);
+};
--- backend/routes/RouteRoutes.js
+++ backend/routes/RouteRoutes.js
@@ -1,3 +1,13 @@
-module.exports = function (app,routeController) {
-    app.get('/route', routeController.getAllRoutes);
-}
+module.exports = function (app, routeController) {
+	app.get("/route", routeController.getAllRoutes);
+	app.get("/route/:id", routeController.getRouteById);
+	app.post("/route", routeController.addRoute);
+	app.put("/route/:id", routeController.updateRoute);
+	app.delete("/route/:id", routeController.deleteRoute);
+	app.post("route/:id/location", routeController.addLocationToRoute);
+	app.delete("route/:id/location", routeController.deleteLocationFromRoute);
+	app.post(
+		"route/:id/changeOrder/:id",
+		routeController.changeOrderOfLocationInRoute
+	);
+};
