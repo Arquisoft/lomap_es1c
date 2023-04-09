9777e8ccadf768660d2aeddc0f505e9160006db6
Miguel <migueelsuarez@gmail.com>
2023-04-09 18:28:01 +0200

    Elimino ñapa

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -61,12 +61,10 @@ export default function App({ logOutFunction }) {
 				.get("http://localhost:8080/location", { withCredentials: true })
 				.then((response) => {
 					if (response.data.length !== data.length) {
-						console.log(response.data);
 						setData(response.data);
 					}
 				})
 				.catch((error) => {
-					//getData();
 					console.log(error);
 				});
 		}
