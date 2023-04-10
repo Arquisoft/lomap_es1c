01a41a3fa5929b0f024fabe6541d7245dcc193be
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-04-10 23:04:32 +0200

    Botones en login

--- my-app/src/login.js
+++ my-app/src/login.js
@@ -1,18 +1,38 @@
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
-import React from 'react';
+import React, { useState } from 'react';
 import "./login.css";
 
-
 export default function Login({logInFunction}){
+
+  const [text, setText] = useState('');
+
+  const handleInrupt = () => {
+    setText('https://login.inrupt.com/');
+  };
+  
+  const handleSolid = () => {
+    setText('https://solidcommunity.net/login');
+  };
+
   return (
-    <div class="mainDiv">
-      <div class="logoYParrafo">
-        <img src="/logoLoMap.png" class="logo" alt="Logo de LoMap"></img>
-        <p class="descripción">LoMap te permite cerar mapas personalizados de los lugares que te interesan.</p>
+    <div className="mainDiv">
+      <div className="logoYParrafo">
+        <img src="/logoLoMap.png" className="logo" alt="Logo de LoMap"></img>
+        <p className="descripción">LoMap te permite cerar mapas personalizados de los lugares que te interesan.</p>
       </div>
-      <div class="formName">
+      <div className="formName">
         <Box className="caja" component="span">
+          <TextField
+            label="Escribir algo"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <br></br>
+          <Button className="btnLogin" onClick={handleInrupt}>Inrupt</Button>
+          <br></br>
+          <Button className="btnLogin" onClick={handleSolid}>Solid</Button>
+          <br></br>
           <Button className="btnLogin" onClick={logInFunction}>Iniciar Sesión</Button>
         </Box>
       </div>
