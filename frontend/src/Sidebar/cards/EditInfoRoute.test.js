a72bd7ca27ab9989f38960f6757d9176187ad851
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-04-11 22:35:31 +0200

    Especifico tests para EditRouteInfo

--- frontend/src/Sidebar/cards/EditInfoRoute.test.js
+++ frontend/src/Sidebar/cards/EditInfoRoute.test.js
@@ -15,7 +15,7 @@ const routeCalls = {
 }
 
 describe('EditRouteInfo', () => {
-    it ("Components have the desired value when creating new route", () => {
+    it ("Create new route with no locations", () => {
         const addMockResponse = {id: 123456789};
         routeCalls.API_addRoute.mockResolvedValue(addMockResponse);
 
@@ -61,7 +61,7 @@ describe('EditRouteInfo', () => {
         expect(routeCalls.API_deleteLocationFromRoute).toHaveBeenCalledTimes(0)
     }),
 
-    it ("Click on return button when creating new route", () => {
+    it ("Return when creating new route with no", () => {
         render(
             <EditRouteInfo
                 route = {null}
@@ -80,4 +80,21 @@ describe('EditRouteInfo', () => {
         expect(routeCalls.API_addLocationToRoute).toHaveBeenCalledTimes(0)
         expect(routeCalls.API_deleteLocationFromRoute).toHaveBeenCalledTimes(0)
     })
+
+    // --------------------------------------------------
+
+    it ("Creating route renders correctly", () => {})
+    it ("Updating route renders correctly", () => {})
+    it ("Can create a route with no places", () => {})
+    it ("Can create a route with some places", () => {})
+    it ("Cannot create a route with no name", () => {})
+    it ("Cannot update a route to have no name", () => {})
+    it ("Places can be added", () => {})
+    it ("Already stored places can be removed", () => {})
+    it ("Just stored places can be removed", () => {})
+    it ("Removed places can be added", () => {})
+    it ("Only needed calls are executed", () => {})
+    it ("No places can be added when there are no places", () => {})
+    it ("No places can be added when there are no places left to add", () => {})
+    it ("Return function doesn't modify the data", () => {})
 })
