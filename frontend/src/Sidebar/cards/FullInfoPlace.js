57a0a1c66ac7bf9a84e9780ea3e490626032c455
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-04-15 19:12:12 +0200

    Render condicional para la información de un lugar

--- frontend/src/Sidebar/cards/FullInfoPlace.js
+++ frontend/src/Sidebar/cards/FullInfoPlace.js
@@ -3,7 +3,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
-import { IconButton, Tooltip } from '@mui/material';
+import { IconButton } from '@mui/material';
 import Rating from '@mui/material/Rating';
 import { useTranslation } from "react-i18next";
 import { Navigation, Pagination } from "swiper";
@@ -51,6 +51,7 @@ const images = [
 export default function FullInfoPlace({place, returnFunction,setPosition, changeDrawerContent, categorias, API_location_calls}) {
     const [t] = useTranslation("global");
     const [loading, setLoading] = useState(false)
+    console.log(place)
 
     function allowEdit() {
         changeDrawerContent(
@@ -83,31 +84,90 @@ export default function FullInfoPlace({place, returnFunction,setPosition, change
         <IconButton onClick={returnFunction}><ArrowBackIcon/></IconButton>
         <h1>{place.name}</h1>
         
-        {place.valoracion ? <Rating value={place.valoracion} readOnly/> : <Rating value={place.valoracion} disabled/>}
-
         <h3>Categoria:</h3>
         <p>{place.category}</p>
 
-        <h3>Comentario:</h3>
-        <p></p>
+        {place.reviews  &&  place.reviews.length>0  &&  
+            <>
+                <h3>Reviews: </h3>
+                <Swiper
+                    pagination={{ type: "fraction", }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                >
+                    {place.reviews.map(
+                        review => (
+                            <SwiperSlide>
+                                <p>{review.author}</p>
+                                <Rating value={(review.rating)/2.0} readOnly/>
+                            </SwiperSlide>
+                        )
+                    )}
+                </Swiper>
+            </>
+        }
+        
+        {place.comments  &&  place.comments.length>0  &&
+            <>
+                <h3>Comentarios:</h3>
+                <Swiper
+                    pagination={{ type: "fraction", }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                >
+                    {place.comments.map(
+                        comment => 
+                        (<SwiperSlide key={comment.id}>
+                            <p>"{comment.text}"({comment.author})</p>
+                        </SwiperSlide>)
+                    )}
+                </Swiper>
 
-        <h3>Fotos:</h3>
-        <Swiper
-            pagination={{ type: "fraction", }}
-            navigation={true}
-            modules={[Pagination, Navigation]}
-        >
-            {/* TODO: reemplazar por las imágenes del lugar */}
-            {images.map(
-                i => 
-                (<SwiperSlide key={i.imgPath+"slide"}><img src={i.imgPath}/></SwiperSlide>)
-            )}
-        </Swiper>
+            </>
+        }
 
-        <Tooltip title={t("sidebar.place.edit")} placement="bottom"><IconButton onClick={allowEdit}><EditIcon/></IconButton></Tooltip>
-        <Tooltip title={t("sidebar.place.locate")} placement="bottom"><IconButton onClick={centerMapToPlace}><TravelExploreIcon/></IconButton></Tooltip>
+        {place.photos  &&  place.photos.length>0  &&
+            <>
+                <h3>Fotos:</h3>
+                <Swiper
+                    pagination={{ type: "fraction", }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                >
+                    {place.photos.map(
+                        photo =>
+                        (<SwiperSlide>
+                            {/* TODO: añadir un SwiperSlide por cada imagen */}
+                            <img src=""/>
+                        </SwiperSlide>)
+                    )}
+                </Swiper>
+            </>
+        }
 
         <br></br>
+
+            <LoadingButton
+				color="secondary"
+                onClick={allowEdit}
+				loading={loading}
+				loadingPosition="start"
+				startIcon={<EditIcon/>}
+				variant="contained"
+			>
+				<span>{t("sidebar.place.edit")}</span>
+			</LoadingButton>
+
+            <LoadingButton
+				color="secondary"
+                onClick={centerMapToPlace}
+				loading={loading}
+				loadingPosition="start"
+				startIcon={<TravelExploreIcon/>}
+				variant="contained"
+			>
+				<span>{t("sidebar.place.locate")}</span>
+			</LoadingButton>
 
 			<LoadingButton
 				color="secondary"
