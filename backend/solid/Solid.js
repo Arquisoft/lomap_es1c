afe04d01569b259a0dd2d244e7017be694c2c9e1
UO284647 <uo284647@uniovi.es>
2023-04-16 15:19:35 +0200

    mejora rendimiento getAllRoutes

--- backend/solid/Solid.js
+++ backend/solid/Solid.js
@@ -185,7 +185,7 @@ async function getAllRoutes(Session, friendWebID) {
 async function getRouteById(Session, idRoute, friendWebID) {
 	let myBaseUrl = await getPodUrlAll(friendWebID, { fetch: Session.fetch });
 	myBaseUrl = myBaseUrl[0];
-	return await routes.getRouteById(Session, idRoute, myBaseUrl);
+	return await routes.getRouteById(Session, idRoute, myBaseUrl, true);
 }
 
 async function deleteRouteById(Session, idRoute, friendWebID) {
